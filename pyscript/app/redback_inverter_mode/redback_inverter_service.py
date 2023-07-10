259227a0a87acf2a881dedd453c6f3bc76a69898
Chris Abberley <26394346+cabberley@users.noreply.github.com>
2023-07-10 12:01:56 +1000

    Create redback_inverter_service.py

---
+++ pyscript/app/redback_inverter_mode/redback_inverter_service.py
@@ -0,0 +1,12 @@
+import redbackfunction
+
+@service
+def set_invertor_mode(email,password, serialNumber, inverterMode, inverterPower, swVersion):
+    if email =='secret':
+        email = pyscript.config['secrets']['redback_email']
+    if password =='secret':
+        password = pyscript.config['secrets']['redback_password']
+    #pyscript.config['secrets']['some_secret']
+    if email =='' or password =='' or serialNumber =='' or inverterMode =='' or swVersion =='':
+        return 'Error: Missing parameter'
+    setInverter = redbackfunction.setInverterMode1(rbEmail= email, rbPassword=password, rbSerialNumber=serialNumber, rbInverterMode=inverterMode, rbInverterPower=inverterPower, rbRossVersion=swVersion)
